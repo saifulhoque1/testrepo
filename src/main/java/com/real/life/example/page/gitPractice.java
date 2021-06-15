5ca337f6e50f8e1d1bfafacc7db203a9091b730e
saifulhoque1 <saifulcu@gmail.com>
2021-06-14 19:59:47 -0700

    Update

--- src/main/java/com/real/life/example/page/gitPractice.java
+++ src/main/java/com/real/life/example/page/gitPractice.java
@@ -4,7 +4,11 @@ public class gitPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//Dummy Change
+		int x  = 10;
+		int y = x+2;
+		System.out.println(y);
+						
 	}
 
 }
